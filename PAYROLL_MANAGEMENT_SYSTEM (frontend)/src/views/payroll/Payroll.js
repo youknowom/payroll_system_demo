e24a1166470b68abba8b04aa7061b8e3271e667c
Omkar <omdb2004@gmail.com>
2024-10-17 22:14:15 +0530

    payroll

--- PAYROLL_MANAGEMENT_SYSTEM (frontend)/src/views/payroll/Payroll.js
+++ PAYROLL_MANAGEMENT_SYSTEM (frontend)/src/views/payroll/Payroll.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 function Payroll() {
-<<<<<<< HEAD
-  return <div>Payroll paise hi paisa</div>
-=======
-  return <div>Payroll paisa hi paias</div>
->>>>>>> ab2e7e38aed88f26a223360bc43d0a4f12e0ae66
+  return <div>Payroll money</div>
 }
 
 export default Payroll
